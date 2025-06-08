ff9194d394e113980bdc264e716bda9f2d5f7f00
arvind021 <ak4392916@gmail.com>
2025-06-08 14:05:30 +0530

    Update sudoers.py

--- AloneX/plugins/sudo/sudoers.py
+++ AloneX/plugins/sudo/sudoers.py
@@ -61,7 +61,7 @@ async def sudoers_list(client, message: Message):
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("𝐍𝐢𝐤𝐚𝐥 𝐑𝐚𝐧𝐝𝐢 𝐁𝐚𝐥𝐚 𝐒𝐮𝐝𝐨𝐥𝐢𝐬𝐭 𝐃𝐞𝐤𝐡𝐧𝐞 𝐀𝐚𝐲𝐚 𝐇𝐚𝐢 𝐛𝐚𝐝𝐚🖕😎😂", show_alert=True)
+        return await callback_query.answer("Chudhary ko papa bol pahale 🖕😎😂", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
